ecd40674d22ff4f60c61c8c178b7fbeb416ccdbd
arloki <arlettes485@gmail.com>
2023-12-15 12:43:45 -0400

    slider 2.5

--- tienda/js/script-tienda.js
+++ tienda/js/script-tienda.js
@@ -48,13 +48,3 @@ function toggle(e) {
     if (this.closest('.has-child').classList != 'expand');
     this.closest('.has-child').classList.toggle('expand');
 }
-
-//slider
-const swiper = new Swiper('.swiper', {
-    
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-  });
